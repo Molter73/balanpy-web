fbdfd57fb10e22c644d4647ff69f3d7228b8ce68
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-05-14 21:44:24 +0200

    Borrar Menú Móvil y Ajustar Menú al Centro

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
         alt="balanpy"
         className="w-[200px] h-[50px]"
       />
-      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+      <ul className="list-none sm:flex hidden justify-center items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
@@ -28,35 +28,6 @@ export default function Navbar() {
       </ul>
       <ButtonPrimary styles={"ml-10"} text={"Iniciar Sesión"} />
       <ButtonSecondary styles={"ml-5"} text={"Registrarse"} />
-
-      <div className="sm:hidden flex flex-1 justify-end items-center">
-        <img
-          src={toggle ? "/close.svg" : "/menu.svg"}
-          alt="menu"
-          className="w-[28px] h-[28px] object-contain"
-          onClick={() => setToggle((prev) => !prev)}
-        />
-      </div>
-
-      <div
-        className={`${
-          toggle ? "flex" : "hidden"
-        } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
-      >
-        <ul className="list-none flex flex-col justify-end items-center flex-1">
-          {navLinks.map((nav, index) => (
-            <li
-              key={nav.id}
-              className={`font-poppins
-            font-normal cursor-pointer text-[16px] ${
-              index === navLinks.length - 1 ? "mr-0" : "mb-4"
-            } text-black`}
-            >
-              <a href={`#${nav.id}`}>{nav.title}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
     </nav>
   );
 }
