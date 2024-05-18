aca3b3c36745e495e1bb56576fe5d121a2e2e378
Brenda Solorzano <bssolben@hotmail.com>
2024-05-18 12:14:30 +0200

    Modificación de pantallas Login y Register

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-        "./src/app/**/*.{html,js}",
-    ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#EAF8F6",
+        secondary: "#0D6C5F",
+        white: "#ffffff",
+        balanpy: {
+          50: "#EAF8F6",
+          100: "#D5F4F0",
+          200: "#C6EBE6",
+          300: "#B1E5DE",
+          400: "#9FE2D9",
+          500: "#87D5CA",
+          DEFAULT: "#70CABD",
+          600: "#70CABD",
+          700: "#48BAAA",
+          800: "#279D8D",
+          900: "#0D6C5F",
+        },
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
     },
   },
-
-  screens: {
-    xs: "480px",
-    ss: "620px",
-    sm: "768px",
-    md: "1060px",
-    lg: "1200px",
-    xl: "1700px",
-  },
-
   plugins: [],
-}
-
+};
