749224b43de5f605896571925d86513a4fe48488
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-05-18 21:51:55 +0200

    Eliminado archivo styles.js innecesario

--- src/components/dashboard/navbar/index.js
+++ src/components/dashboard/navbar/index.js
@@ -1,12 +1,10 @@
-import {styles} from "./styles"
-
 import CurrentDateTime from "./date-time"
 import SearchBar from "./searchbar"
 import UserData from "./user-data"
 
 export default function Navbar() {
     return <nav className="bg-balanpy-50">
-        <div className={styles.root}>
+        <div className="max-w-screen-xl max-h-[5rem] flex items-center justify-between mx-auto p-4">
             <div className="flex flex-col">
                 <div className="text-emerald-800 font-bold text-2xl">
                     Hola Alejandro
--- src/components/dashboard/navbar/searchbar/index.js
+++ src/components/dashboard/navbar/searchbar/index.js
@@ -1,15 +1,13 @@
-import {styles} from "../styles"
-
 export default function SearchBar() {
     return (
             <div className="flex">
                 <div className="relative hidden md:block">
-                    <div className={styles.search.root}>
+                    <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="text" id="search-navbar" className={styles.search.input} placeholder="CAMBIAR MASCOTA" />
+                    <input type="text" id="search-navbar" className="block w-full p-2 ps-10 text-sm text-gray-300 border border-gray-300 rounded-full bg-balanpy-50" placeholder="CAMBIAR MASCOTA" />
                 </div>
             </div>
     )
