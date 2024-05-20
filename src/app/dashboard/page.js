cb59c3e56b45105e9a6c38940b616b085e9e5972
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-05-20 21:38:49 +0200

    Mejora Visual Panel Mascota

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -13,51 +13,77 @@ import {
 } from "@/constants/dashboard"
 
 export default function Dashboard() {
-    return <DashboardLayout>
+    return (
+      <DashboardLayout className="h-full bg-balanpy-50">
         <div className="text-black p-6 bg-balanpy-50 grid gap-4 grid-cols-5 grid-rows-2">
-            <Container styles="col-span-4">
-                <div className="grid gap-4 grid-cols-4 grid-rows-1">
-                    <PetInfo />
-                    <div className="flex flex-col text-center align-center justify-center items-center col-span-3">
-                        <span className="text-xl text-balanpy-800 font-bold">¿Qué quieres hacer?</span>
-                        <div className="flex flex-row">
-                            <ActivityButton text="Añadir Comida" image="/dashboard/mascotas.svg" alt_text="añadir comida" />
-                            <ActivityButton text="Añadir Baño" image="/dashboard/bano.svg" alt_text="añadir baño" />
-                            <ActivityButton text="Añadir Cepillado" image="/dashboard/cepillo.svg" alt_text="añadir cepillado" />
-                            <ActivityButton text="Añadir Paseo" image="/dashboard/paseo.svg" alt_text="añadir paseo" />
-                        </div>
-                        <span className="text-xl text-balanpy-800 font-bold">¿Qué información necesitas?</span>
-                        <div className="flex flex-row p-2">
-                            <InformationButton text="Visualizar DNI Mascota" />
-                            <InformationButton text="Visualizar Pedigree" />
-                            <InformationButton text="Visualizar Esterilizado" />
-                            <InformationButton text="Visualizar Archivo PPP" />
-                        </div>
-                        <div className="flex flex-row">
-                            <InformationButton text="Historial de Alergias" />
-                            <InformationButton text="Historial de Vacunas" />
-                            <InformationButton text="Historial de Enfermedades" />
-                        </div>
-                        <div className="flex flex-row align-center justify-center items-center bg-balanpy-800 rounded-xl p-4">
-                            <img src="/dashboard/paw.svg" alt="huella" />
-                            <span className="text-balanpy-50 p-2 font-bold text-xl">EDITAR INFORMACIÓN</span>
-                        </div>
-                    </div>
+          <Container styles="col-span-4">
+            <div className="grid gap-2 p-4 grid-cols-4 grid-rows-1">
+              <PetInfo />
+              <div className="flex flex-col text-center align-center justify-center items-center col-span-3">
+                <span className="font-poppins text-xl text-balanpy-800 font-bold text-[24px] mb-2">
+                  ¿Qué quieres hacer?
+                </span>
+                <div className="flex flex-row">
+                  <ActivityButton
+                    text="Añadir Comida"
+                    image="/dashboard/mascotas.svg"
+                    alt_text="añadir comida"
+                  />
+                  <ActivityButton
+                    text="Añadir Baño"
+                    image="/dashboard/bano.svg"
+                    alt_text="añadir baño"
+                  />
+                  <ActivityButton
+                    text="Añadir Cepillado"
+                    image="/dashboard/cepillo.svg"
+                    alt_text="añadir cepillado"
+                  />
+                  <ActivityButton
+                    text="Añadir Paseo"
+                    image="/dashboard/paseo.svg"
+                    alt_text="añadir paseo"
+                  />
                 </div>
-            </Container>
-            <Container styles="col-span-1">
-                <WeatherInfo />
-            </Container>
-            <Container styles="col-span-4">
-                <Activities />
-            </Container>
-            <Container styles="col-span-1">
-                <div className="flex flex-col text-center align-center justify-center items-center p-2">
-                    <h2 className="text-gray-400 font-semibold text-3xl p-1">Mascotas</h2>
-                    <Pet icon={dog_icon} type="Perros" count="1" />
-                    <Pet icon={cat_icon} type="Gatos" count="0" />
+                <span className="font-poppins text-xl text-balanpy-800 font-bold text-[24px] mb-2 mt-6">
+                  ¿Qué información necesitas?
+                </span>
+                <div className="flex flex-row p-2">
+                  <InformationButton text="Visualizar DNI Mascota" />
+                  <InformationButton text="Visualizar Pedigree" />
+                  <InformationButton text="Visualizar Esterilizado" />
+                  <InformationButton text="Visualizar Archivo PPP" />
                 </div>
-            </Container>
+                <div className="flex flex-row mt-1">
+                  <InformationButton text="Historial de Alergias" />
+                  <InformationButton text="Historial de Vacunas" />
+                  <InformationButton text="Historial de Enfermedades" />
+                </div>
+                <div className="flex flex-row align-center justify-center items-center bg-balanpy-800 rounded-full py-2 px-12 mt-10 cursor-pointer hover:bg-balanpy-900 transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-slate-300 ">
+                  <img src="/dashboard/paw.svg" alt="huella" />
+                  <span className="font-poppins text-balanpy-50 p-1 font-bold text-xl ml-4">
+                    EDITAR INFORMACIÓN
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Container>
+          <Container styles="col-span-1">
+            <WeatherInfo />
+          </Container>
+          <Container styles="col-span-4">
+            <Activities />
+          </Container>
+          <Container styles="col-span-1">
+            <div className="flex flex-col text-center align-center justify-center items-center p-2">
+              <h2 className="text-gray-400 font-semibold text-3xl p-1">
+                Mascotas
+              </h2>
+              <Pet icon={dog_icon} type="Perros" count="1" />
+              <Pet icon={cat_icon} type="Gatos" count="0" />
+            </div>
+          </Container>
         </div>
-    </DashboardLayout>
+      </DashboardLayout>
+    );
 }
--- src/components/dashboard/activity-button/index.js
+++ src/components/dashboard/activity-button/index.js
@@ -1,8 +1,16 @@
 export default function ActivityButton({text, image, alt_text}) {
     return (
-        <div className="flex flex-col items-center justify-center align-center p-3">
-            <img src={image} alt={alt_text} height="64" width="64" className="border rounded-xl border-balanpy-800 bg-balanpy-50 p-3" />
-            <span className="text-black">{text}</span>
-        </div>
-    )
+      <div className="flex flex-col items-center justify-center align-center p-4">
+        <img
+          src={image}
+          alt={alt_text}
+          height="64"
+          width="64"
+          className="border rounded-xl border-balanpy-800 bg-balanpy-50 p-3 h-[64px] w-[64px] mb-2 cursor-pointer hover:bg-white transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-slate-300"
+        />
+        <span className="text-black font-poppins font-regular text-[14px]">
+          {text}
+        </span>
+      </div>
+    );
 }
