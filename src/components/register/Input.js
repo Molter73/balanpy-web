ab7cd8e93ae400d780479745cd6b1c5833e4a62a
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-05-21 11:21:20 +0200

    Retoques en Apartado Registro

--- src/components/register/Input.js
+++ src/components/register/Input.js
@@ -2,7 +2,7 @@ export const Input = ({ id, placeholder }) => {
     return(
             <input
                 id={id}
-                className=" mt-0 block w-full px-2 py-1 font-poppins font-normal bg-transparent border border-slate-200 rounded-lg text-[14px] text-balanpy-900 shadow-sm placeholder-gray
+                className="block w-full px-2 py-2 font-poppins font-normal bg-transparent border border-slate-200 rounded-lg text-[14px] text-balanpy-900 shadow-sm placeholder-gray mt-1
                 focus:outline-none focus:border-balanpy focus:ring-1 focus:ring-slate-200"
                 placeholder={placeholder}
             />
