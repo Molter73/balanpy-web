765945d633432caf932f39dae2a8cfe6f78a5685
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-05-23 16:48:57 +0200

    Creada Pantalla Dashboard para Veterinarios (#13)
    * Creada Pantalla Dashboard para Veterinarios
    
    * Mejora legibilidad del código y ajustes

--- src/components/dashboard/container/index.js
+++ src/components/dashboard/container/index.js
@@ -1,6 +1,6 @@
 export default function Container({styles, children}) {
     return (
-        <div className={`flex flex-col bg-white align-center justify-between items-between border-[3px] border-balanpy-800 rounded-[25px] p-2 ${styles}`} >
+        <div className={`flex flex-col bg-white align-center justify-between items-between border-[3px] border-balanpy-800 rounded-[25px] ${styles}`} >
             {children}
         </div>
     )
