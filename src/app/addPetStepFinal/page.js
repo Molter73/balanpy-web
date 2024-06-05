72c879839f419a24f59221566500820d3b546901
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-06-05 13:40:07 +0200

    Paso 4 Arreglado

--- src/app/addPetStepFinal/page.js
+++ src/app/addPetStepFinal/page.js
@@ -4,11 +4,15 @@ import styles from "../style";
 
 export default function Home() {
   return (
-    //BLOQUE PRINCIPAL DE COMPONENTES Y COLOR DE FONDO
     <main className="justify-items-center grid grid-cols-1 p-20 min-h-screen bg-gradient-to-b from-balanpy-500 from-20% via-balanpy-700 via-65% to-balanpy-900 to-100%">
       <div className="justify-center">
         <div className="flex justify-center w-auto p-1">
-          <img src="pets/LogoBalanpyBlanco.png" alt="Balanpy Logo" width={120} height={120}></img>
+          <img
+            src="pets/LogoBalanpyBlanco.png"
+            alt="Balanpy Logo"
+            width={120}
+            height={120}
+          ></img>
         </div>
         <div className="flex justify-center p-4">
           <h1 className={`${styles.heading2} text-white text-center `}>
@@ -24,21 +28,29 @@ export default function Home() {
         <Steps index="4" styles={"bg-balanpy-800 text-white"} />
       </section>
 
-      
-      <section className={`${styles.paddingX} ${styles.flexCenter} mx-4 max-w-[1900px]`} >
-        <div className={`${styles.boxWidthPets} px-10 pb-2 rounded-2xl w-full bg-white`}>
+      <section className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className="py-16 mx-auto rounded-[50px] w-4/5 bg-white">
           <div className={`${styles.padding}`}>
-              <p className={`${styles.heading1} text-center font-bold text-balanpy-800`}>¡Felicidades!</p>
+            <p
+              className={`${styles.heading1} text-center font-bold text-balanpy-800`}
+            >
+              ¡Felicidades!
+            </p>
           </div>
-          <div className="flex justify-center">
-            <p className={`${styles.heading4} text-center tracking-widest text-balanpy-800 w-2/4`}>
-                Has dado el primer paso hacia un cuidado 
-                más completo y amoroso para tu mascota registrándola en Balanpy. Estamos emocionados de acompañarte en esta aventura de 
-                bienestar y felicidad para tu amigo de cuatro patas. ¡Bienvenido a la familia Balanpy!
-            </p>            
+          <div className="flex justify-center items-center w-full">
+            <p
+              className={`text-center  text-balanpy-800 w-2/3 text-[24px] font-medium leading-10`}
+            >
+              Has dado el primer paso hacia un cuidado más completo y amoroso
+              para tu mascota registrándola en Balanpy. Estamos emocionados de
+              acompañarte en esta aventura de bienestar y felicidad para tu
+              amigo de cuatro patas. ¡Bienvenido a la familia Balanpy!
+            </p>
           </div>
           <div className="flex justify-center m-10 gap-6 ">
-            <ButtonPrimary styles={"text-[30px] bg-balanpy-800 border-balanpy-800"} text={"Vuelve al Panel de Control"} href="/dashboard"/>
+            <button className="py-4 px-10 font-poppins font-medium text-[24px] rounded-[10px] outline-none border-4 border-balanpy-800 box-border transition-all duration-300 ease-in-out text-primary bg-balanpy-800 hover:bg-white hover:text-balanpy-800">
+              Vuelve al Panel de Control
+            </button>
           </div>
         </div>
       </section>
