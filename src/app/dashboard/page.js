f69bd15e767b6870fa43acb43574cedd7b193f4d
bmsjale93 <117013312+bmsjale93@users.noreply.github.com>
2024-06-05 10:44:25 +0200

    Dashboard con Mascotas Arreglada

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -67,11 +67,11 @@ function generatePet(pet) {
 
   return <>
     <PetInfo image={pet.image} status={pet.status} />
-    <div className="flex flex-col text-center align-center justify-center items-center col-span-3">
-      <span className="text-xl text-balanpy-800 font-bold text-[24px] mb-2">
-        ¿Qué quieres hacer?
+    <div className="flex flex-col text-center justify-center items-center col-span-3 h-full">
+      <span className=" text-balanpy-800 font-bold text-[24px] mb-2">
+        ¿Qué quieres hacer ahora?
       </span>
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-center items-center">
         <ActivityButton
           text="Añadir Comida"
           image="/dashboard/mascotas.svg"
@@ -101,16 +101,16 @@ function generatePet(pet) {
           modalData={walkActivity}
         />
       </div>
-      <span className="text-xl text-balanpy-800 font-bold text-[24px] mb-2 mt-6">
+      <span className="text-balanpy-800 font-bold text-[24px] mb-2 mt-6">
         ¿Qué información necesitas?
       </span>
-      <div className="flex flex-row p-2">
+      <div className="flex flex-row justify-center items-center p-2">
         <InformationButton text="Visualizar DNI Mascota" />
         <InformationButton text="Visualizar Pedigree" />
         <InformationButton text="Visualizar Esterilizado" />
         <InformationButton text="Visualizar Archivo PPP" />
       </div>
-      <div className="flex flex-row mt-1">
+      <div className="flex flex-row justify-center items-center mt-1">
         <InformationButton text="Historial de Alergias" />
         <InformationButton text="Historial de Vacunas" />
         <InformationButton text="Historial de Enfermedades" />
@@ -163,7 +163,7 @@ export default async function Dashboard() {
     <DashboardLayout className="h-full bg-balanpy-50">
       <div className="text-black p-6 bg-balanpy-50 grid gap-4 grid-cols-5 grid-rows-2">
         <Container styles="col-span-4">
-          <div className="grid gap-2 p-4 grid-cols-4 grid-rows-1">
+          <div className="grid gap-2 grid-cols-4 grid-rows-1 justify-center items-center h-full p-10">
             {petComponent}
           </div>
         </Container>
@@ -173,9 +173,9 @@ export default async function Dashboard() {
         <Container styles="col-span-4 p-8">
           <ActivityList activities={activities} />
         </Container>
-        <Container styles="col-span-1 h-full">
-          <div className="flex flex-col text-center align-top justify-center items-start p-2 h-full">
-            <h2 className="text-gray-400 font-semibold text-2xl text-center p-1 w-full mb-4">
+        <Container styles="col-span-1 h-full px-6 py-6">
+          <div className="flex flex-col text-center align-middle justify-center items-start p-2 h-full">
+            <h2 className="text-gray-400 font-semibold text-2xl text-center w-full mb-4">
               Mascotas
             </h2>
             {pets.map((pet, index) => (
@@ -186,11 +186,11 @@ export default async function Dashboard() {
                 cantidad={pet.cantidad}
               />
             ))}
-            <Link href="/dashboard/edit-pet" className="no-underline">
-              <div className="flex flex-row align-center justify-center items-center bg-balanpy-800 rounded-full py-2 px-12 mt-10 cursor-pointer hover:bg-balanpy-900 transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-slate-300">
+            <Link href="/dashboard/edit-pet" className="no-underline w-full">
+              <div className="flex flex-row w-full align-center justify-center items-center bg-balanpy-800 rounded-full py-2 px-6 mt-10 cursor-pointer hover:bg-balanpy-900 transition-all duration-300 ease-in-out hover:shadow-lg hover:shadow-slate-300">
                 <img src="/dashboard/paw.svg" alt="huella" />
-                <span className="text-balanpy-50 p-1 font-bold text-xl ml-4">
-                  EDITAR INFORMACIÓN
+                <span className="text-balanpy-50 p-1 font-bold text-[15px] ml-4 py-2">
+                  Editar Mascotas
                 </span>
               </div>
             </Link>
--- src/components/dashboard/pet-info/index.js
+++ src/components/dashboard/pet-info/index.js
@@ -2,7 +2,7 @@ import StatusBar from "@/components/dashboard/statusbar"
 
 export default function PetInfo({ image, status }) {
   return (
-    <div className="flex flex-col col-span-1 border-[3px] rounded-[20px] border-balanpy-800 align-center justify-center items-center p-4 text-black font-regular">
+    <div className="flex flex-col col-span-1 border-[3px] rounded-[20px] border-balanpy-800 align-center justify-center items-center p-4 py-10 text-black font-regular h-full">
       <img src={image.src} alt={image.alt} className="rounded-full mb-2" />
       {status.map((stat, i) => {
         let color = "bg-balanpy-600"
